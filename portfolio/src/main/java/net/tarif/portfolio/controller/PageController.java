0a1ba395f190bd76f582cfaf918cbe6f9e06e2c6
tarif aljnidi <tarifaljnidi1986@hotmail.com>
2019-05-05 15:05:36 +0100

    Using online amazon database

---
+++ portfolio/src/main/java/net/tarif/portfolio/controller/PageController.java
@@ -0,0 +1,22 @@
+package net.tarif.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("greeting", "welcom");
+
+		return mv;
+	}
+
+}
